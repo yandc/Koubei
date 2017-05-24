844ce2d12c12084c10659e2384d7b2fa3c79f1c0
search <search@recommend-14.(none)>
2017-05-24 19:15:01 +0800

    url unquote for skuIds

--- Koubei/view.py
+++ Koubei/view.py
@@ -3,13 +3,14 @@
 from django.http import HttpResponse
 from ctrl import *
 import json
+import urllib
 
 def getKoubei(request):
     page = 0
     pagesize = 10
     res = {'code':-1, 'msg':'Param error!', 'data':None}
     try:
-        skuIds = request.GET['skuIds']
+        skuIds = urllib.unquote(request.GET['skuIds'])
         if 'page' in request.GET:
             page = int(request.GET['page'])
         if 'pagesize' in request.GET:

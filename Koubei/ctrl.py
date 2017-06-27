b28807661d10106d2878de5a1ad8794841545e91
search <search@recommend-14.(none)>
2017-06-27 14:01:23 +0800

    add abtest

--- Koubei/ctrl.py
+++ Koubei/ctrl.py
@@ -3,6 +3,7 @@
 from redis_util import *
 import datetime
 import time
+import zlib
 
 def getSortedKoubei(skuIds, start, end, debug, source, dvcId):
     res = {'code':0, 'msg':'Succ', 'data':None}
@@ -13,8 +14,21 @@ def getSortedKoubei(skuIds, start, end, debug, source, dvcId):
     
     result = []
     rds = RedisUtil(env='online')
+    abTest = rds.get_obj('kbrank:abtest')
+    strategy = 0
+    if abTest and type(abTest) == list:
+        hint = zlib.crc32(dvcId)&0xffffffff
+        left = hint % sum(abTest)
+        for i, v in enumerate(abTest):
+            if left < v:
+                break
+            left -= v
+        strategy = i
     for itemId in relateSet:
-        key = 'koubei:rank_score:%s'%itemId
+        if strategy == 0:
+            key = 'koubei:rank_score:%s'%itemId
+        else:
+            key = 'koubei:rank_score:%s:%s'%(strategy, itemId)
         li = rds.get_obj(key)
         if not li:
             continue
@@ -37,4 +51,4 @@ def getSortedKoubei(skuIds, start, end, debug, source, dvcId):
     return res
 
 if __name__ == '__main__':
-    print getSortedKoubei('1185761', 0, 10, True)
+    print getSortedKoubei('1185761', 0, 10, True, 'test', 'abc')
